dc50c4b580af11afd8f6bdd5c0c09013396025a0
Emasker <shuangxu21@qq.com>
2022-05-08 21:01:25 +0800

    html

---
+++ Vue/01_basic/15.单文件组件/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <School></School>
+  <Student></Student>
+  </div>
+</template>
+
+<script>
+import Student from './Student'
+import School from './School'
+export default {
+    name:'App',
+    components:{
+        School,
+        Student
+    }
+}
+</script>
---
+++ Vue/01_basic/15.单文件组件/Student.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h2 ref="hi">欢迎</h2>
+        <h2>学生姓名：{{studentName}}</h2>
+            <h2>学生年龄：{{age}}</h2>
+            <button @click="show">点击</button>
+        </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+       data() {
+            return {
+                studentName: 'breeze',
+                age: 18,
+            }
+        },
+        methods: {
+            show(){
+                console.log(this.$refs.hi);
+            }
+        },
+}
+</script>
